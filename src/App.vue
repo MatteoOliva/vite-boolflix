72b325bb67da719e34e0840d32f693d0aa3d7883
MatteoOliva <oliva.matteo98@outlook.com>
2024-02-18 16:45:10 +0100

    STO IMPAZZENDO FINISCE COSI, E' STATO BELLO

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script>
+import axios from "axios";
+import { store } from "./store";
+
+export default {
+  data() {
+    return {
+      store,
+      searchedTerm: "ritorno al futuro",
+    };
+  },
+
+  methods: {
+    performSearch() {
+      axios
+        .get(this.store.api.uri + "/search/movie", {
+          params: {
+            api_key: store.api.key,
+            query: this.searchedTerm,
+          },
+        })
+        .then((response) => {
+          console.log(response.data);
+        });
+    },
+  },
+};
+</script>
+
+<template></template>
+
+<style lang="scss">
+@use "./styles/general.scss";
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,13 @@
+import {reactive} from 'vue';
+
+export const store = reactive({
+
+    api: {
+        Uri: 'https://api.themoviedb.org/3',
+        key: '1bb35cb20bd1a933c2be62d80e9395dd',
+
+    }
+    
+   
+});
+
